cccd8843772fabd2d0bafe2212502fc0de4aec7e
Danniel Hugo <dannielhugo@gmail.com>
2022-07-04 15:23:36 -0300

    ci(enviroment): add multiple enviroments to firebase

---
+++ .github/scripts/decrypt_secret_next.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
+--output $GITHUB_WORKSPACE/.env.next .env.next.gpg
